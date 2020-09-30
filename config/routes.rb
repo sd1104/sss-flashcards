0cda20fdabda176313c70462f78e2683ecf133f2
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-09-30 17:36:04 +0900

    routes set

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  get 'genre/index'
-  get 'words/index'
   root to: 'home#index'
+  resources :genre, only: :index
+  resources :words, only: :index
+  namespace :api do
+    namespace :v1 do
+      resources :words, only: :index
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
