0af709431f9b9fcfdf38238381f2aac82ea06e60
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-09-30 19:14:19 +0900

    Update cards_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,24 @@
 class CardsController < ApplicationController
+  def new
+    @genres = Genre.all
+    @cards = CardCollection.new
+  end
+
+  def create
+    @cards = CardCollection.new(cards_params)
+    if @cards.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def show
     @card = Card.find(params[:id])
+  end
+
+  private
+  def cards_params
+    params.require(:cards)
   end
 end
