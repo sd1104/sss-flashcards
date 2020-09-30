8275941b4a9a24d61ce924081e5bcf9e504a0f3b
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-09-30 17:37:14 +0900

    api/v1/words controller set

--- app/controllers/api/v1/words_controller.rb
+++ app/controllers/api/v1/words_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::WordsController < ApplicationController
+  protect_from_forgery
+
+  rescue_from Exception, with: :render_status_500
+
+  rescue_from ActiveRecord::RecordNotFound, with: :render_status_404
+
+  def index
+    words = Word.all
+    render json: words
+  end
+
+  private
+
+  def render_status_404(exception)
+    render json: { errors: [exception] }, status: 404
+  end
+
+  def render_status_500(exception)
+    render json: { errors: [exception] }, status: 500
+  end
 end
